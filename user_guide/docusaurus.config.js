2c3a67b3ce2cc3185a05e608486dca60a2286f44
Amar <amar@mpsc.io>
2022-10-03 01:16:44 +0100

    Revert "branding to be realtive"
    This reverts commit 4055c93efad8a7ecb0c46edc26652b0b104fb59a.

--- user_guide/docusaurus.config.js
+++ user_guide/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'HooT',
   tagline: 'The World is a Conference! Collaboration for Everyone!',
   url: 'https://mpsc-io.github.io',
-  baseUrl: '/branding/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/hoot_icon.png',
